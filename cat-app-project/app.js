315a59d09a2c39cb6827be7cd46030d49b742bb8
brittanyleonard <Belah615@gmail.com>
2020-12-07 19:32:08 -0600

    working on carousel

--- cat-app-project/app.js
+++ cat-app-project/app.js
@@ -8,7 +8,7 @@ $(() => {
            console.log(data);
            $("#facts").html(data.all);
         })
-   $.ajax({
+   $.ajax({ //want to make a random cat image but might just need to have the images individually 
       url:'https://api.thecatapi.com/v1/images/search'
    }).then(
       (image) => {
