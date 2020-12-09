8b838dd4198b8a000bd31c5107c8bbf47c4b46b1
brittanyleonard <Belah615@gmail.com>
2020-12-08 23:19:54 -0600

    updated css

--- cat-app-project/app.js
+++ cat-app-project/app.js
@@ -1,25 +1,44 @@
 $(() => {
 
-   let arrayIndex = $('#fact').children().attr('text')
-
-
-
+   // let arrayIndex = $('#fact').children().attr('text')
    $.ajax({
          url:'https://cat-fact.herokuapp.com/facts/random',
       }).then(
          (data) => {
            //handler for success
-           console.log(data);
+           // console.log(data);
            $("#fact").html(data.text);
         })
+
+   $('.next').on('click', ()=>{
+
+   $.ajax({
+         url:'https://cat-fact.herokuapp.com/facts/random',
+      }).then(
+         (data) => {
+           // console.log(data);
+           $("#fact").html(data.text);
+        })
+   });
+
+   $('.previous').on('click', ()=>{
+
+   $.ajax({
+         url:'https://cat-fact.herokuapp.com/facts/random',
+      }).then(
+         (data) => {
+           // console.log(data);
+           $("#fact").html(data.text);
+        })
+   });
+
 
   // array of random images
   let imgIndex = 0;
   let numOfImages = $('.carousel-image').children().length -1;
-  console.log(numOfImages);
+  // console.log(numOfImages);
 
   $('.next').on('click', () => {
-
     $('.carousel-image').children().eq(imgIndex).css('display', 'none');
     if(imgIndex < numOfImages) {
       imgIndex++;
